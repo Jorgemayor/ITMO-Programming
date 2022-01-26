17609c469fc66cf0128782d37844b2233a82c35c
Jorgemayor <jorge.e.mayor@gmail.com>
2022-01-26 16:11:09 +0300

    First test

--- Lab3-МайорХорхе/src/main.java
+++ Lab3-МайорХорхе/src/main.java
@@ -1,7 +1,30 @@
+import livingBeings.Plant;
+import livingBeings.TypePlants;
+import places.Place;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class main {
 
     public static void main(String[] args) {
 
+        try {
+            Place bosque = new Place("bosque", new ArrayList<Plant>() {
+                {
+                    new Plant(TypePlants.LEMON_TREE, "12/01/2022");
+                    new Plant(TypePlants.MANGO_TREE, "12/01/2022");
+                    new Plant(TypePlants.ORANGE_TREE, "12/01/2022");
+                    new Plant(TypePlants.HONEY_TREE, "12/01/2022");
+                    new Plant(TypePlants.CARROT_BUSH, "12/01/2022");
+                    new Plant(TypePlants.MARGARITA, "12/01/2022");
+                }
+            });
+        } catch (ParseException exception) {
+            System.out.print(exception.getMessage());
+        }
 
 
     }
